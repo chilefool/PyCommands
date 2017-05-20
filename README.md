d00e593ce5cbeb6bee1269278ac959c4716a2225
Bobby Noell <chilefool@strayprocess.com>
2017-05-19 22:30:16 -0400

    Update tlsloader to run under Immunity Debugger 1.74+
    In v1.74 of Immunity Debugger, immlib function names were standardized
    to camelCase which broke the previous version of `tlsloader`.  I've
    updated it to work on the current debugger version (v1.85 at this
    time).
    
    `tlsloader` now also works on Windows 7 (previously only tested on
    XP).  When running on Windows 7, it's not necessary to modify the
    initial breakpoint setting.

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ The new file handle will be shown in the message box at the bottom of the window
 
 Loads a PE file, sets breakpoints on all TLS callback functions, and stops execution at the first TLS callback function. Type `!tlsloader <path_to_file>` in the command box to run it. Additional details will be written to the Log window (Alt + L).
 
-Usage of this PyCommand requires you to configure the debugger to make its first pause at the system breakpoint (which not the default setting).  To make this change, go to "Options" -> "Debugging options" -> [Events].
+Usage of this PyCommand on Windows XP requires you to configure the debugger to make its first pause at the system breakpoint (which not the default setting). To make this change, go to "Options" -> "Debugging options" -> [Events].  This should not be necessary on Windows 7.
 
-If the PE file doesn't contain any TLS callback functions, execution will be paused at the entry point as defined in the PE header.
+If the PE file doesn't contain any TLS callback functions, execution will be paused at the entry point as defined in the debugger settings.
 
 _Note:_ this script does not currently work on DLLs.
--- tlsloader.py
+++ tlsloader.py
@@ -1,109 +1,106 @@
 #!/usr/bin/env python
 """
-Immunity Debugger PyCommand that opens a PE file and sets breakpoints on all TLS
-  callback functions
+PyCommand that opens an EXE and sets breakpoints on all TLS callback functions
 
-Author: bobby@strayprocess.com
+Place within PyCommands directory and type "!tlsloader <path_to_file>" in
+the command bar in order to load the PE file, set breakpoints on all TLS
+callback functions, and stop execution at the first TLS callback function.
+Additional details will be written to the Log window (Alt + L).
 
-Usage: Place within PyCommands directory and type "!tlsloader <path_to_file>" in
-  the command bar in order to load the PE file, set breakpoints on all TLS
-  callback functions, and stop execution at the first TLS callback function.
-  Additional details will be written to the Log window (Alt + L).
-  
-  Usage of this PyCommand requires you to configure the debugger to make its
-  first pause at the system breakpoint (which not the default setting).  To make
-  this change, go to "Options" -> "Debugging options" -> [Events].  I don't
-  believe that the python API allows me to do this automatically (or at least I
-  didn't find it).
+Usage of this PyCommand on Windows XP requires you to configure the debugger to
+make its first pause at the system breakpoint (which not the default setting).
+To make this change, go to "Options" -> "Debugging options" -> [Events].  This
+should not be necessary on Windows 7.
 
-  If the PE file doesn't contain any TLS callback functions, execution will be
-  paused at the entry point as defined in the PE header.  Note: this script does
-  not currently work on DLLs.
+If the PE file doesn't contain any TLS callback functions, execution will be
+paused at the entry point as defined in the debugger settings.
 
-Revision History:
-  4/22/2010, v1.0 - Initial public release
-
+Note: this script does not currently work on DLLs.
 """
-
-__VERSION__ = '1.0'
-
 import immlib
 import pefile
 
-DESC= "Open a PE file and set breakpoints on all TLS callback functions"
+__VERSION__ = '1.1'
+NAME = 'tlsloader'
+DESC = 'Open an EXE and set breakpoints on all TLS callback functions'
+COPYRIGHT = 'Copyright (c) 2010-2017 Bobby Noell'
+LICENSE = 'MIT'
 
-def main(args): 
+
+def set_breakpoints(imm, callback_functions):
+    """Set breakpoints on each callback function
+
+    :param imm: Debugger instance
+    :param callback_functions: list of callback addresses
+    :return: None
+    """
+    for idx in range(len(callback_functions)):
+        imm.setBreakpoint(callback_functions[idx])
+        imm.setComment(callback_functions[idx], 'TLS callback {}'.format(idx))
+        imm.setLabel(callback_functions[idx], 'TLS_callback_{}'.format(idx))
+
+
+def main(args):
+    """PyCommand that opens an EXE and sets breakpoints on all TLS callback functions
+
+    :param args: arguments passed to the PyCommand
+    :return: status string
+    """
     imm = immlib.Debugger()
-    imm.Log("")
+    imm.log('')
     if not args:
-        imm.Log("[ !tlsloader ]")
-        imm.Log("  Usage: !tlsloader <path_to_file>", gray=True)
-        return "Usage: !tlsloader <path_to_file>"
+        imm.log('[ !tlsloader ]')
+        return 'Usage: !tlsloader <path_to_file>'
     filename = args[0]
 
-    # open the PE in Immunity Debugger
-    imm.openProcess(filename)
-
-    imm.Log("")
-    imm.Log("[ !tlsloader -- %s ]" % filename)
+    imm.log('')
+    imm.log('[ !tlsloader -- {} ]'.format(filename))
 
     # load with pefile
     try:
-        pe = pefile.PE(name = filename)
-    except IOError:
-        imm.Log("  Unable to open %s" % filename, gray=True)
-        return "Unable to open %s" % filename
+        pe = pefile.PE(name=filename)
+    except IOError as exception:
+        return 'Error: Unable to open {} ({})'.format(filename, exception.message)
     except pefile.PEFormatError:
-        imm.Log("  Unable to parse PE file: %s" % arg, gray=True)
-        return "Unable to parse PE file: %s" % arg
+        return 'Error: Unable to parse {} as a PE file'.format(filename)
 
     # exit if attempting to load a DLL
     if 0x2000 & pe.FILE_HEADER.Characteristics == 0x2000:
-        imm.Log("  tlsloader cannot be used on DLL files", gray=True)
-        return "tlsloader cannot be used on DLL files"
+        return 'Error: tlsloader cannot be used on DLL files'
 
+    callback_functions = []
     # make sure that the PE actually has a TLS directory
-    if hasattr(pe, "DIRECTORY_ENTRY_TLS"):
-        imm.Log("  Array of TLS callback functions at 0x%08x"
-            % pe.DIRECTORY_ENTRY_TLS.struct.AddressOfCallBacks, gray=True)
-
-        callback_array_rva = pe.DIRECTORY_ENTRY_TLS.struct.AddressOfCallBacks - pe.OPTIONAL_HEADER.ImageBase 
+    if hasattr(pe, 'DIRECTORY_ENTRY_TLS'):
+        imm.log(
+            '  Array of TLS callback functions at 0x{:08x}'.format(pe.DIRECTORY_ENTRY_TLS.struct.AddressOfCallBacks),
+            gray=True)
+        callback_array_rva = pe.DIRECTORY_ENTRY_TLS.struct.AddressOfCallBacks - pe.OPTIONAL_HEADER.ImageBase
 
         # read the array of TLS callbacks until we hit a NULL ptr (end of array)
         idx = 0
-        callback_functions = [ ]
         while pe.get_dword_from_data(pe.get_data(callback_array_rva + 4 * idx, 4), 0):
             callback_functions.append(pe.get_dword_from_data(pe.get_data(callback_array_rva + 4 * idx, 4), 0))
+            imm.log('    * TLS callback function {} defined at 0x{:08x}'.format(idx, callback_functions[idx]),
+                    gray=True)
             idx += 1
-
         # if we start with a NULL ptr, then there are no callback functions
         if idx == 0:
-            # get the address of the entry point so we can skip to it
-            ep = pe.OPTIONAL_HEADER.ImageBase + pe.OPTIONAL_HEADER.AddressOfEntryPoint
-            imm.Log("    * No TLS callback functions supported... executing until 0x%08x (EP)"
-                % ep, gray=True)
-            imm.Run(ep)
-            return "No TLS callback functions supported"            
-        else:
-            for idx in range(len(callback_functions)):
-                imm.Log("    * TLS callback function %i defined at 0x%08x"
-                    % (idx, callback_functions[idx]), gray=True)
-                imm.setBreakpoint(callback_functions[idx])
-                imm.setComment(callback_functions[idx], "TLS callback %i" % idx)
-                imm.setLabel(callback_functions[idx], "TLS_callback_%i" % idx)
-            imm.Log("  Executing until first breakpoint", gray=True)
-            
-            # run until we hit a breakpoint (should be our first callback)
-            imm.Run()
-            return "Execution paused at first TLS callback function"
+            imm.log('    * No TLS callback functions defined', gray=True)
 
-    else:
-        # get the address of the entry point so we can skip to it
-        ep = pe.OPTIONAL_HEADER.ImageBase + pe.OPTIONAL_HEADER.AddressOfEntryPoint
-        imm.Log("  No TLS Directory found... executing until 0x%08x (EP)"
-            % ep, gray=True)
-        imm.Run(ep)
-        return "No TLS directory found"
-   
-if __name__=="__main__":
-    print "This module is for use within Immunity Debugger only"     
+    imm.openProcess(filename)
+    if callback_functions:
+        if imm.getOsVersion() == 'xp':
+            # ensure we get to system breakpoint
+            imm.run(callback_functions[0])
+        else:
+            # should pause at ntdll!RtlUserThreadStart on Win7
+            imm.pause()
+
+        set_breakpoints(imm, callback_functions)
+        imm.run(callback_functions[0])
+        imm.pause()
+        return 'Execution paused at first TLS callback function'
+    return 'No TLS callback functions found'
+
+if __name__ == '__main__':
+    print 'This module is for use within Immunity Debugger only'
